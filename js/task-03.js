36c90ffca2081118a5b6a0d7c765b121429ef3e1
olechka-moiseenko <73791863+olechka-moiseenko@users.noreply.github.com>
2021-04-11 17:27:08 +0300

    Новое решение 3 задания

--- js/task-03.js
+++ js/task-03.js
@@ -1,23 +1,30 @@
+// Напиши скрипт для создания галлереи изображений по массиву данных.
+// В HTML есть список ul#gallery.
+// <ul id = "gallery" ></ul >
+//   Используй массив объектов images для создания тегов img вложенных в li.
+// Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
 const images = [
-    {
-      url:
-        'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      alt: 'White and Black Long Fur Cat',
-    },
-    {
-      url:
-        'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-    },
-    {
-      url:
-        'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      alt: 'Group of Horses Running',
-    },
-  ];
+  {
+    url:
+      "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
 
-  const style =  document.querySelector("style");
-  style.textContent += `
+const style = document.querySelector("style");
+style.textContent += `
   .flexBox{
     list-style: none;
     display: flex;
@@ -28,13 +35,11 @@ const images = [
     width: 320px;
     height: 240px;
   }`;
-
-  const listItems = images.map(i => {
-    const li = document.createElement('li');
-    li.insertAdjacentHTML('beforeend', `<img class="imageStyle" src="${i.url}" alt="${i.alt}"></img>`);
-    return li;
-  });
-  const ul = document.querySelector("ul#gallery");
-  ul.classList.add('flexBox');
-  ul.append(...listItems);
- 
+const ul = document.querySelector("ul#gallery");
+ul.classList.add("flexBox");
+let innerHtml = "";
+images.forEach(
+  (i) =>
+    (innerHtml += `<li><img class="imageStyle" src="${i.url}" alt="${i.alt}"></img></li>`)
+);
+ul.insertAdjacentHTML("afterbegin", innerHtml);
